68ca296836976ca11ba7c9d4b62e7cd5b142f93b
Luckyman64 <clement.lantiat@gmail.com>
2022-10-14 12:29:23 +0200

    commit 14/10/2022

--- app/src/androidTest/java/api.kt
+++ app/src/androidTest/java/api.kt
@@ -1,2 +1,12 @@
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
 class api {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.themoviedb.org/3/")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    val service = retrofit.create(Tmdbapi::class.java)
 }
